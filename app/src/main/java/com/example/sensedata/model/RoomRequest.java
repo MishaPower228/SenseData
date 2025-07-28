55268f7cae761b35ee9dd5bc54324345ca2293ad
Misha Stepanec <stepanecmisha@gmail.com>
2025-07-28 15:20:17 +0300

    fdjsfjsdfj

---
+++ app/src/main/java/com/example/sensedata/model/RoomRequest.java
@@ -0,0 +1,13 @@
+package com.example.sensedata.model;
+
+public class RoomRequest {
+    private String name;
+    private String imageName;
+
+    public RoomRequest(String name, String imageName) {
+        this.name = name;
+        this.imageName = imageName;
+    }
+
+    // гетери й сетери (можна згенерувати автоматично)
+}
